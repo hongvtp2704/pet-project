1036ff685354d1504f4ea3cad622454b976807e9
pinky <hongvtp.2704@gmail.com>
2023-06-28 15:49:05 +0700

    add env variable to vite.config.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app: Express = express()
-const port : string | undefined = process.env.PORT
 
 app.get('/', (req: Request, res: Response)=> {
     res.send('Hello word')
-})
-
-app.set((port!), ()=> {
-    console.log(`Server is running at port: ${port}`)
 })
 
 export const viteNodeApp = app;
